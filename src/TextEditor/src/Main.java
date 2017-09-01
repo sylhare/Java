51fd7f50d8ffa8bbc74bad8bb2abc9eb405fbc9a
sylhare <sylhareng@gmail.com>
2017-09-01 14:34:26 -0400

    Added TextEditor

---
+++ src/TextEditor/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Main class of the text editor, when the main() is a run, the text editor will show up.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        Editor app = new Editor();
+        app.setVisible(true);
+    }
+}
