55016f4ae0db496f3c5d6b683d800a72a6c1f437
sylhare <sylhareng@gmail.com>
2017-12-14 09:27:54 -0500

    Testing gradle

--- src/gradle-build-scan-quickstart/src/test/java/example/ExampleTest.java
+++ src/gradle-build-scan-quickstart/src/test/java/example/ExampleTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
-
     @Test
     public void testJoin() {
         assertEquals("Joined string didn't match", "1 2 3", Example.join("1", "2", "3"));
