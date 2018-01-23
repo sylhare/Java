48d04a032a25b043e190f35c3e672c366931b87b
sylhare <sylhareng@gmail.com>
2018-01-23 17:48:05 -0500

    Refactoring Cucumber

--- src/cucumber-gradle/src/test/java/skeleton/CakesTest.java
+++ src/cucumber-gradle/src/test/java/skeleton/CakesTest.java
@@ -5,10 +5,12 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "html:./src/test/resources/reporting/default-cucumber-report/html",
-        "json:./src/test/resources/reporting/cucumber-report.json", "pretty:./src/test/resources/reporting/default-cucumber-report/cucumber-pretty.txt",
-        "usage:./src/test/resources/reporting/default-cucumber-report/cucumber-usage.json", "junit:./src/test/resources/reporting/default-cucumber-report/cucumber-results.xml" },
-        features = { "./src/test/resources/features/" })
-        //glue = { "./src/test/java/skeleton/" })
+@CucumberOptions(plugin = { "html:./cucumber/default-cucumber-report/html",
+                            "json:./cucumber/cucumber-report.json",
+                            "pretty:./cucumber/default-cucumber-report/cucumber-pretty.txt",
+                            "usage:./cucumber/default-cucumber-report/cucumber-usage.json",
+                            "junit:./cucumber/default-cucumber-report/cucumber-results.xml" },
+                features = { "./src/test/resources/features/" })
+                //glue = { "./src/test/java/skeleton/" })
 public class CakesTest {
 }
